03168d8b34e82f04867760e108a23d90c35dc80f
Deivid Kirchev <58637632+DeividKirchev@users.noreply.github.com>
2025-03-21 08:42:02 +0200

    (fix): Fixing entry block. Renaming setActiveConfigurationController

--- controllers/configuration/setActiveConfiguration.js
+++ controllers/configuration/setActiveConfiguration.js
@@ -2,12 +2,25 @@ const ActiveConfiguration = require("../../models/configuration/activeConfigurat
 const ChatBlock = require("../../models/configuration/chatBlockModel");
 const Configuration = require("../../models/configuration/configurationModel");
 const restifyErrors = require("restify-errors");
-const createAndSetActiveConfiguration = async (req, res) => {
+const crypto = require("crypto");
+
+const setActiveConfiguration = async (req, res) => {
+  console.log(req.body);
   const blocks = req.body.blocks;
+
+  if (!blocks || blocks.length === 0) {
+    throw new restifyErrors.BadRequestError(
+      `Cannot set active configuration. Missing blocks.`
+    );
+  }
 
   blocks.forEach((block) => {
     block.originalNextBlock = block.nextBlock;
     block.originalId = block._id;
+
+    if (!block.originalId) {
+      block.originalId = crypto.randomUUID();
+    }
 
     delete block.nextBlock;
     delete block._id;
@@ -57,15 +70,31 @@ const createAndSetActiveConfiguration = async (req, res) => {
     await ChatBlock.bulkSave(updatedBlocks);
   }
 
+  // Set entry block
+  let entryBlock = insertedBlocks.find(
+    (block) => block.originalId === req.body.entryBlock
+  );
+  if (!entryBlock && req.body.entryBlock) {
+    throw new restifyErrors.BadRequestError(
+      `Cannot set entry block. Missing block with id ${req.body.entryBlock}`
+    );
+  }
+  if (!entryBlock) {
+    entryBlock = insertedBlocks.find(
+      (block) => block.originalId === blocks[0].originalId
+    );
+  }
+
   const configuration = new Configuration({
     blocks: insertedBlocks,
+    entryBlock: entryBlock._id,
   });
   const savedConfiguration = await configuration.save();
 
   const activeConfiguration = await ActiveConfiguration.findOneAndUpdate(
     {},
     { $set: { configuration: savedConfiguration._id } },
-    { upsert: true }
+    { upsert: true, returnOriginal: false }
   ).populate({
     path: "configuration",
     populate: {
@@ -108,4 +137,4 @@ const mapNextBlocks = (
   return false;
 };
 
-module.exports = createAndSetActiveConfiguration;
+module.exports = setActiveConfiguration;
