2da85c1e936013a20ef225da900c95896b450c7b
Deivid Kirchev <58637632+DeividKirchev@users.noreply.github.com>
2025-03-21 08:42:02 +0200

    (fix): Fixing entry block. Renaming setActiveConfigurationController

--- routes/configurationRouter.js
+++ routes/configurationRouter.js
@@ -1,8 +1,8 @@
 var router = new (require("restify-router").Router)();
 const getActiveConfiguration = require("../controllers/configuration/getActiveConfiguration");
-const createAndSetActiveConfiguration = require("../controllers/configuration/createAndSetActiveConfiguration");
+const setActiveConfiguration = require("../controllers/configuration/setActiveConfiguration");
 
 router.get("", getActiveConfiguration);
-router.post("", createAndSetActiveConfiguration);
+router.post("", setActiveConfiguration);
 
 module.exports = router;
